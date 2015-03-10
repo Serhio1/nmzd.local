f60ee4e63bb5e29a9aa1188211f2a28b72511716
Serhio1 <sergej.krikovcov@gmail.com>
2015-03-10 22:55:39 +0200

    added rendering for checkboxMatrx form element

--- Src/Modules/Nmkd/Forms/Elements/CheckboxMatrix/CheckboxMatrix.php
+++ Src/Modules/Nmkd/Forms/Elements/CheckboxMatrix/CheckboxMatrix.php
@@ -9,6 +9,15 @@ class CheckboxMatrix extends Checkbox {
     protected $_attributes = array("type" => "checkbox");
     protected $inline;
 
+    public function getCSSFiles() {
+        $cssUrl = Router::buildUrl('Src/Modules/Nmkd/Forms/Elements/CheckboxMatrix/css/checkbox_matrix.css');
+        return array(
+            $cssUrl
+        );
+    }
+    
+    public function getJSFiles() {}
+    
     public function render() { 
         if(isset($this->_attributes["value"])) {
             if(!is_array($this->_attributes["value"]))
@@ -25,7 +34,32 @@ class CheckboxMatrix extends Checkbox {
                 $labelClass .= " inline";
 
         $count = 0;
-        foreach($this->options as $value => $text) {
+        $i = 0;
+        $checkStr = '';
+        echo '<table class="checkbox-matrix table table-bordered">';
+        echo '<thead><tr><th></th>';
+        foreach ($this->options['horizontal'] as $horValue => $horText) {
+            echo '<th>' . $horText . '</th>'; 
+        }
+        echo '</tr></thead><tbody>';
+        
+        foreach ($this->options['vertical'] as $vertValue => $vertText) {
+            echo '<tr>';
+            echo '<td>' . $vertText . '</td>';
+            foreach ($this->options['horizontal'] as $horValue => $horText) {
+                echo '<td>' . '<input id="' . $vertValue . '-' . $horValue . '" class="check-matrix-element hide" type=checkbox><label for="' . $vertValue . '-' . $horValue . '"></label>' . '</td>'; 
+            }
+            /*while ($i < count($this->options['horizontal'])) {
+                $checkStr .= '<td></td>';
+                echo $checkStr;
+                $i++;
+            }*/
+            
+            echo '</tr>';
+        }
+        
+        echo '</tbody></table>';
+        /*foreach($this->options as $value => $text) {
             $value = $this->getOptionValue($value);
 
             echo '<label class="', $labelClass, '"> <input id="', $this->_attributes["id"], '-', $count, '"', $this->getAttributes(array("id", "value", "checked", "required")), ' value="', $this->filter($value), '"';
@@ -33,7 +67,7 @@ class CheckboxMatrix extends Checkbox {
                     echo ' checked="checked"';
             echo '/> ', $text, ' </label> ';
             ++$count;
-        }	
+        }*/	
     }
 }
 
